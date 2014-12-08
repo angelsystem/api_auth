513e061a4c04cb8a5012eb5906a7f82400a27d03
angelito rojas <angelsystem1387@gmail.com>
2014-12-08 12:23:40 -0500

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,12 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    p User.all
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App"
       redirect_to @user
     else
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -45,7 +45,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-   test "password should have a minimum length" do
+  test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
