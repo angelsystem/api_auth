c39625f919159ea067b9cce6bd7915886d3881f6
angelito rojas <angelsystem1387@gmail.com>
2014-12-08 15:50:20 -0500

    Finish user edit, update, index, adn destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,15 @@
 module SessionsHelper
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+
   def log_in user
     session[:user_id] = user.id
   end
@@ -38,5 +49,9 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def current_user?(user)
+    user == current_user
   end
 end
